1fa5b3c4cadfe2e89e310245ea5c63c3b65bd7bf
arunsuku4web <arun.greenfrog@gmail.com>
2019-01-01 09:26:42 +0530

    Dummy data using factory

--- app/Question.php
+++ app/Question.php
@@ -8,6 +8,13 @@ class Question extends Model
 {
     public function user()
     {
-        return $this->belongTo(User::class);
+        return $this->belongsTo(User::class);
+    }
+    protected $fillable =['title','body'];
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title']=$value;
+        $this->attributes['slug']=str_slug($value);
+
     }
 }
